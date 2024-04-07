50f06f24e7affb77280a08eab8388eac29f1a55f
2pvq2 <tinono0948@gmail.com>
2024-04-07 23:04:20 +0700

    update wed

--- main-project/booking/script.js
+++ main-project/booking/script.js
@@ -7,21 +7,29 @@ const bookings = document.querySelector('.bookings');
 const booking = document.querySelector('.booking');
 const home =document.querySelector('.home');
 const intro = document.querySelector('.intro');
+const fields = document.querySelector('.fields');
+const dtn = document.querySelector('.dtn');
+
 
 registerlink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
     booking.classList.remove('active-booking');
+    intro.classList.remove('active-intro');
 });
 
 loginlink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
     booking.classList.remove('active-booking');
+    intro.classList.remove('active-intro');
     
 });
 
 popup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
     booking.classList.remove('active-booking');
+    dtn.classList.remove('active-dtn');
+
+    
     
 });
 
@@ -33,14 +41,22 @@ bookings.addEventListener('click', ()=> {
     booking.classList.toggle('active-booking');
     wrapper.classList.remove('active-popup');
     intro.classList.remove('active-intro');
+    dtn.classList.remove('active-dtn');
 });
 
 home.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
     intro.classList.toggle('active-intro');
     booking.classList.remove('active-booking');
+    dtn.classList.remove('active-dtn');
 });
 
+fields.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+    intro.classList.remove('active-intro');
+    booking.classList.remove('active-booking');
+    dtn.classList.toggle('active-dtn');
+});
 
 
 
